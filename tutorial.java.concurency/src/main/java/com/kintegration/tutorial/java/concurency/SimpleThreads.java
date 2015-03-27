094abddc355c93ee388c87f59bfdc2a86ce94e06
LMMagbun1 <LMMagbun1@q.com>
2015-03-27 14:05:05 -0700

    baseproject

---
+++ tutorial.java.concurency/src/main/java/com/kintegration/tutorial/java/concurency/SimpleThreads.java
@@ -0,0 +1,85 @@
+package com.kintegration.tutorial.java.concurency;
+
+public class SimpleThreads {
+	
+	// Display a message, preceded by 
+	// the name of the current thread
+	static void threadMessage(String message) {
+		String threadName =
+				Thread.currentThread().getName();
+		System.out.format("%s: %s%n", threadName, message);
+	}
+	
+	private static class MessageLoop implements Runnable {
+		
+		public void run() {
+			String importantInfo[] = {
+					"Mares eat cats",
+					"Does eat cats",
+					"Little lambs eat ivy",
+					"A kid will eat ivy too"
+			};
+			System.out.println("Running the message loop hread");
+			try {
+				for (int i = 0; i < importantInfo.length; i++) {
+					//Pause fro 4 seconds
+					Thread.sleep(4000);
+					// Print a message 
+					threadMessage(importantInfo[i]);
+				}
+			} catch (InterruptedException e) {
+				threadMessage("I wasn't done!!!");
+			}
+			
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		//Delay in milliseconds before
+		// we interrupt MessageLoop
+		// thread (default one hour);
+		
+		long patience = 1000 *60 * 60;
+		
+		// If command line argument
+		// present, gives patience
+		// in seconds
+		
+		if (args.length > 0) {
+			try {
+				patience = Long.parseLong(args[0]) * 1000;
+			} catch (NumberFormatException e) {
+				System.err.println("Argument must be an integer");
+				System.exit(1);
+			}
+		}
+		
+		threadMessage("Starting MessageLoop thread");
+		long startTime = System.currentTimeMillis();
+		Thread t = new Thread(new MessageLoop());
+		t.start();
+		
+		while (t.isAlive()) {
+			threadMessage("Still waiting...");
+			// Wait maximum of 1 second
+			// for MessageLoop thread
+			// to finish.
+			t.join(1000);
+			//Thread.currentThread().join();
+			System.out.format("Time elapsed is: %d and patience is: %d%n", (System.currentTimeMillis() - startTime), patience);
+			if (((System.currentTimeMillis() - startTime ) > patience )
+					&& t.isAlive()) {
+				threadMessage("Tired of waiting!!!");
+				t.interrupt();
+				// Shouldn't be long now
+				// -- Wait indefinitely
+				t.join();
+			}
+		}
+		threadMessage("Finally");
+	}
+
+}
---
+++ tutorial.java.concurency/src/main/java/com/kintegration/tutorial/java/concurency/producerconsumer/ProducerConsumerExample.java
@@ -0,0 +1,15 @@
+package com.kintegration.tutorial.java.concurency.producerconsumer;
+
+public class ProducerConsumerExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Drop drop = new Drop();
+		(new Thread(new Producer(drop))).start();
+		(new Thread(new Consumer(drop))).start();
+	}
+
+}
